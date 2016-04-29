OCA/account-financial-reporting

[FIX][mis_builder] Fix the creation of a new mis.report.instance.

mis_builder/models/mis_builder.py
@@ -667,12 +667,12 @@ class MisReportInstance(models.Model):
 
     @api.multi
     def display_settings(self):
-        self.ensure_one()
+        assert len(self._ids) <= 1
         view_id = self.env.ref('mis_builder.mis_report_instance_view_form')
         return {
             'type': 'ir.actions.act_window',
             'res_model': 'mis.report.instance',
-            'res_id': self.id,
+            'res_id': self.id if self.id else False,
             'view_mode': 'form',
             'view_type': 'form',
             'views': [(view_id.id, 'form')],

OCA/mis-builder

mis_builder/models/mis_builder.py
@@ -629,12 +629,12 @@ class MisReportInstance(models.Model):
 
     @api.multi
     def display_settings(self):
-        self.ensure_one()
+        assert len(self.ids) <= 1
         view_id = self.env.ref('mis_builder.mis_report_instance_view_form')
         return {
             'type': 'ir.actions.act_window',
             'res_model': 'mis.report.instance',
-            'res_id': self.id,
+            'res_id': self.id if self.id else False,
             'view_mode': 'form',
             'view_type': 'form',
             'views': [(view_id.id, 'form')],