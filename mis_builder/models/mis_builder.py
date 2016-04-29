acsone/account-financial-reporting

[IMP][mis_builder] MIS Builder widget improvement, add print and export button in the widget.

mis_builder/models/mis_builder.py
@@ -658,6 +658,32 @@ class MisReportInstance(models.Model):
         }
 
     @api.multi
+    def print_pdf(self):
+        self.ensure_one()
+        data = {'context': self.env.context}
+        return {
+            'name': 'MIS report instance QWEB PDF report',
+            'model': 'mis.report.instance',
+            'type': 'ir.actions.report.xml',
+            'report_name': 'mis_builder.report_mis_report_instance',
+            'report_type': 'qweb-pdf',
+            'context': self.env.context,
+            'data': data,
+        }
+
+    @api.multi
+    def export_xls(self):
+        self.ensure_one()
+        return {
+            'name': 'MIS report instance XLS report',
+            'model': 'mis.report.instance',
+            'type': 'ir.actions.report.xml',
+            'report_name': 'mis.report.instance.xls',
+            'report_type': 'xls',
+            'context': self.env.context,
+        }
+
+    @api.multi
     def compute(self):
         assert len(self) == 1
 

OCA/mis-builder

mis_builder/models/mis_builder.py
@@ -604,6 +604,30 @@ class MisReportInstance(models.Model):
         }
 
     @api.multi
+    def print_pdf(self):
+        self.ensure_one()
+        return {
+            'name': 'MIS report instance QWEB PDF report',
+            'model': 'mis.report.instance',
+            'type': 'ir.actions.report.xml',
+            'report_name': 'mis_builder.report_mis_report_instance',
+            'report_type': 'qweb-pdf',
+            'context': self.env.context,
+        }
+
+    @api.multi
+    def export_xls(self):
+        self.ensure_one()
+        return {
+            'name': 'MIS report instance XLSX report',
+            'model': 'mis.report.instance',
+            'type': 'ir.actions.report.xml',
+            'report_name': 'mis.report.instance.xlsx',
+            'report_type': 'xlsx',
+            'context': self.env.context,
+        }
+
+    @api.multi
     def compute(self):
         assert len(self) == 1
 