c6d8f6fcfce021899a88c411989fec23702886b8
Martronic SA <info@martronic.ch>
2017-06-23 13:40:26 +0200

    Update migration script to 10.0.3.1.0

---
+++ mis_builder/migrations/10.0.3.1.0/post-migration.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# © 2017 ACSONE SA/NV (<http://acsone.eu>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from openupgradelib import openupgrade
+
+
+def migrate(env, version):
+    cr = env.cr
+    map_payment_type(cr)
+
+@openupgrade.migrate(use_env=True)
+def migrate(env, version):
+    cr = env.cr
+    model = env['mis.report.instance']
+    openupgrade.m2o_to_x2m(cr, model, 'mis_report_instance', 'company_ids', 'company_id')
