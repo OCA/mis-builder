ee13d1be01fbaeeddf2cccc90de60f8d336dc466
mreficent <miquel.raich@eficent.com>
2017-11-10 16:35:09 +0100

    [FIX] Fill some defaults in migration script

--- mis_builder/migrations/8.0.2.0.0/post-migration.py
+++ mis_builder/migrations/8.0.2.0.0/post-migration.py
@@ -2,6 +2,27 @@
 # © 2017 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
+from openerp import api, SUPERUSER_ID
+
+
+def fill_defaults(env):
+    cr = env.cr
+
+    default_specs = {
+        'mis.report.instance.period': ['report_instance_id'],
+        'mis.report.kpi': ['report_id'],
+        'mis.report.query': ['report_id'],
+        'mis.report.instance': ['company_id'],
+    }
+
+    for model in default_specs.keys():
+        obj = env[model]
+        for field in default_specs[model]:
+            value = obj.default_get([field])
+            if value:
+                cr.execute("UPDATE %s SET %s = %s WHERE %s is NULL" % (
+                    obj._table, field, value, field))
+
 
 def migrate(cr, version):
     cr.execute("""
@@ -22,3 +43,6 @@ def migrate(cr, version):
         UPDATE mis_report_instance_period
         SET mode='relative'
     """)
+    # set defaults in now required fields
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    fill_defaults(env)
