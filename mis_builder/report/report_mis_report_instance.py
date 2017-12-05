cd59d9b57b8ffa2063e94af08f05390b99cd2cf0
Mathias Francke <mathias.francke@initos.com>
2017-12-05 13:53:59 +0100

    Aligned the code to fit to the changes made in 9.0 branch

--- mis_builder/report/report_mis_report_instance.py
+++ mis_builder/report/report_mis_report_instance.py
@@ -50,7 +50,7 @@ class Report(models.Model):
                                            context=context)
 
     @api.v8
-    def get_pdf(self, records, report_name, html=None, data=None):
-        return Report.get_pdf(self._model, self._cr, self._uid, records.ids,
-                              report_name, html=html, data=data,
-                              context=self._context)
+    def get_pdf(self, docids, report_name, html=None, data=None):
+        return self._model.get_pdf(self._cr, self._uid,
+                                   docids, report_name,
+                                   html=html, data=data, context=self._context)
