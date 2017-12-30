612d9ff653fb6e17527c2be5910d48bb69a36c95
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2017-12-30 21:15:19 +0100

    [MIG] remove some useless diffs with v10

--- mis_builder/__manifest__.py
+++ mis_builder/__manifest__.py
@@ -9,7 +9,8 @@
     'summary': """
         Build 'Management Information System' Reports and Dashboards
     """,
-    'author': 'ACSONE SA/NV, Odoo Community Association (OCA)',
+    'author': 'ACSONE SA/NV,'
+              'Odoo Community Association (OCA)',
     'website': 'https://github.com/OCA/mis-builder/',
     'depends': [
         'account',
--- mis_builder/tests/common.py
+++ mis_builder/tests/common.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # Copyright 2017 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
 import odoo
 
 
