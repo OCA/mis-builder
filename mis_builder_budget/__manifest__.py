c6280025746f8d9c50d54730c184ab40e1b2def5
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2018-11-16 21:54:45 +0100

    10.0.3.3.0

--- mis_builder_budget/__manifest__.py
+++ mis_builder_budget/__manifest__.py
@@ -6,7 +6,7 @@
     'name': 'MIS Builder Budget',
     'summary': """
         Create budgets for MIS reports""",
-    'version': '10.0.3.2.1',
+    'version': '10.0.3.3.0',
     'license': 'AGPL-3',
     'author': 'ACSONE SA/NV, '
               'Odoo Community Association (OCA)',
