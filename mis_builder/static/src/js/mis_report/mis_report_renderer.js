eb43981feb0759d85b893bf3acb203c85772788b
mpanarin <m.panarin@mobilunity.com>
2018-11-28 11:44:19 +0200

    fixup! wip refactor generic JS

--- mis_builder/static/src/js/mis_report/mis_report_renderer.js
+++ mis_builder/static/src/js/mis_report/mis_report_renderer.js
@@ -4,25 +4,22 @@ odoo.define('web.MisReportRenderer', function (require) {
     var BasicRenderer = require('web.BasicRenderer');
 
     var MisReportRenderer =  BasicRenderer.extend({
-        // className: "o_mis_report_view",
         template: 'MisReportView',
         events: _.extend({}, BasicRenderer.prototype.events, {
             'click .mis_builder_drilldown': '_drilldown',
             'click .oe_mis_builder_print': '_print_pdf',
             'click .oe_mis_builder_export': '_export_xls',
-            'click .oe_mis_builder_settings': '_display_settings',
             'click .oe_mis_builder_refresh': '_refresh',
         }),
 
         _getReportData: function (){
             var self = this;
-            var id = this.getParent().initialState.res_id;
             var context = this.getParent().initialState.context;
             return this._rpc({
                 model: 'mis.report.instance',
                 method: 'compute',
-                args: [id],
-                context: context,
+                args: [self.instance_id],
+                context: self.context,
             }).then(function (result) {
                 self.mis_report_data = result;
             });
@@ -30,18 +27,19 @@ odoo.define('web.MisReportRenderer', function (require) {
 
         _getSettings: function () {
             var self = this;
-            var context = this.getParent().initialState.context;
             return this._rpc({
                 model: 'res.users',
                 method: 'has_group',
                 args: ['account.group_account_user'],
-                context: context,
+                context: self.instance_context,
             }).then(function (result) {
                 self.show_settings = result;
             });
         },
 
         willStart: function () {
+            this.instance_id = this.getParent().initialState.res_id;
+            this.instance_context = this.getParent().initialState.context;
             return $.when(
                 this._getReportData(),
                 this._getSettings(),
@@ -49,8 +47,46 @@ odoo.define('web.MisReportRenderer', function (require) {
             );
         },
 
-        start: function() {
-            this._super.apply(this, arguments);
+
+        _refresh: function () {
+            this.replace();
+        },
+
+        _print_pdf: function () {
+            var self = this;
+            this._rpc({
+                model: 'mis.report.instance',
+                method: 'print_pdf',
+                args: [self.instance_id],
+                context: self.instance_context,
+            }).then(function (result) {
+                self.do_action(result);
+            });
+        },
+
+        _export_xls: function () {
+            var self = this;
+            this._rpc({
+                model: 'mis.report.instance',
+                method: 'export_xls',
+                args: [self.instance_id],
+                context: self.instance_context,
+            }).then(function (result) {
+                self.do_action(result);
+            });
+        },
+
+        _drilldown: function (event) {
+            var self = this;
+            var drilldown = $(event.target).data("drilldown");
+            this._rpc({
+                model: 'mis.report.instance',
+                method: 'drilldown',
+                args: [self.instance_id, drilldown],
+                context: self.instance_context,
+            }).then(function (result) {
+                self.do_action(result);
+            });
         },
 
     });
