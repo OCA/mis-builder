a8b530c73cc0c896229a1cee6643f2ab8b190ec6
Juan Jose Scarafia <jjs@adhoc.com.ar>
2019-01-25 16:24:25 -0300

    [REF] add files headers

--- mis_builder_cash_flow/models/mis_cash_flow_forecast_line.py
+++ mis_builder_cash_flow/models/mis_cash_flow_forecast_line.py
@@ -1,3 +1,5 @@
+# Copyright 2019 ADHOC SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from odoo import fields, models, api, _
 from odoo.exceptions import ValidationError
 
--- mis_builder_cash_flow/report/mis_cash_flow.py
+++ mis_builder_cash_flow/report/mis_cash_flow.py
@@ -1,3 +1,5 @@
+# Copyright 2019 ADHOC SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from odoo import api, fields, models, tools
 from psycopg2.extensions import AsIs
 
