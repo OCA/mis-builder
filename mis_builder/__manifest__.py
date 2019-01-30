a29b2d66ea2b235c50e65d048a06a6450be7b91f
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2019-01-31 00:11:48 +0100

    12.0.3.3.0

--- mis_builder/__manifest__.py
+++ mis_builder/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'MIS Builder',
-    'version': '12.0.3.2.2',
+    'version': '12.0.3.3.0',
     'category': 'Reporting',
     'summary': """
         Build 'Management Information System' Reports and Dashboards
