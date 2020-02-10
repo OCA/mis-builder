5466f35c2c053b3cf9da6c481a9c976000e8e372
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2020-02-10 20:04:26 +0100

    [ADD] subreports

--- mis_builder/models/__init__.py
+++ mis_builder/models/__init__.py
@@ -3,6 +3,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from . import mis_report
+from . import mis_report_subreport
 from . import mis_report_instance
 from . import mis_report_style
 from . import aep
