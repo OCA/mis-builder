4b0e3f31e4516208783777c81863be9a157be769
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2020-02-15 17:55:44 +0100

    Use first publication year in copyright notice
    For easier mainteance

--- mis_builder/report/__init__.py
+++ mis_builder/report/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2014-2018 ACSONE SA/NV (<http://acsone.eu>)
+# Copyright 2014 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from . import mis_report_instance_qweb
--- mis_builder/tests/test_mis_safe_eval.py
+++ mis_builder/tests/test_mis_safe_eval.py
@@ -1,4 +1,4 @@
-# Copyright 2016-2018 ACSONE SA/NV (<http://acsone.eu>)
+# Copyright 2016 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 import odoo.tests.common as common
