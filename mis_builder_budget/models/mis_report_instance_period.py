0268beac499a60cd0e302e6198bc2a3489d1d4f2
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2020-02-28 16:45:08 +0100

    [FIX] mis_builder_budget: use _get_additional_move_line_filter
    Use _get_additional_move_line_filter as
    default behavior of _get_additional_budget_item_filter
    so analytic filters work better by default

--- mis_builder_budget/models/mis_report_instance_period.py
+++ mis_builder_budget/models/mis_report_instance_period.py
@@ -38,5 +38,5 @@ class MisReportInstancePeriod(models.Model):
         Returns an Odoo domain expression (a python list)
         compatible with mis.budget.item."""
         self.ensure_one()
-        filters = self._get_filter_domain_from_context()
+        filters = self._get_additional_move_line_filter()
         return filters
