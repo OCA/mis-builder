38f75413687e79867840c9e6f86ee207394fed86
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2020-10-08 17:43:27 +0200

    Mark modules not installable

--- mis_builder/__manifest__.py
+++ mis_builder/__manifest__.py
@@ -28,7 +28,7 @@
         "report/mis_report_instance_xlsx.xml",
     ],
     "qweb": ["static/src/xml/mis_report_widget.xml"],
-    "installable": True,
+    "installable": False,
     "application": True,
     "license": "AGPL-3",
     "development_status": "Production/Stable",
