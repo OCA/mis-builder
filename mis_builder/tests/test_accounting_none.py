dedd9fbcb2f3c02bfa95ce260e75a1e9bc72baf7
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2021-01-13 19:18:10 +0100

    [FIX] doctests

--- mis_builder/tests/test_accounting_none.py
+++ mis_builder/tests/test_accounting_none.py
@@ -1,11 +1,8 @@
 # Copyright 2014 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-import doctest
 
 from ..models import accounting_none
+from .common import load_doctests
 
-
-def load_tests(loader, tests, ignore):
-    tests.addTests(doctest.DocTestSuite(accounting_none))
-    return tests
+load_tests = load_doctests(accounting_none)
