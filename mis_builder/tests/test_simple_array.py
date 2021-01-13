b8beedcd120a2e7c08c7a224f68c9d336d6532a4
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2021-01-13 19:18:10 +0100

    doctests

--- mis_builder/tests/test_simple_array.py
+++ mis_builder/tests/test_simple_array.py
@@ -1,11 +1,7 @@
 # Copyright 2014 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-import doctest
-
 from ..models import simple_array
+from .common import load_doctests
 
-
-def load_tests(loader, tests, ignore):
-    tests.addTests(doctest.DocTestSuite(simple_array))
-    return tests
+load_tests = load_doctests(simple_array)
