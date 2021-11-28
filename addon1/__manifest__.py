9b1d29d6be0d6a2cdbc520aeed0d29f7f7f05a79
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2021-11-28 17:19:31 +0100

    test pre-commit

---
+++ addon1/__manifest__.py
@@ -0,0 +1,7 @@
+{
+    "name": "addon 1",
+    "version": "15.0.1.0.0",
+    "author": "sbi, Odoo Community Association (OCA)",
+    "license": "LGPL-3",
+    "website": "https://github.com/OCA/mis-builder",
+}
