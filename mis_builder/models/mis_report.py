e4ad2044b1b8e41073ed82eb9420a49085c152ac
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2022-02-16 18:57:22 +0100

    [IMP] remove u string prefixes

--- mis_builder/models/mis_report.py
+++ mis_builder/models/mis_report.py
@@ -139,7 +139,7 @@ class MisReportKpi(models.Model):
     def name_get(self):
         res = []
         for rec in self:
-            name = u"{} ({})".format(rec.description, rec.name)
+            name = "{} ({})".format(rec.description, rec.name)
             res.append((rec.id, name))
         return res
 
@@ -166,7 +166,7 @@ class MisReportKpi(models.Model):
             for expression in kpi.expression_ids:
                 if expression.subkpi_id:
                     exprs.append(
-                        u"{}\xa0=\xa0{}".format(
+                        "{}\xa0=\xa0{}".format(
                             expression.subkpi_id.name, expression.name
                         )
                     )
@@ -303,7 +303,7 @@ class MisReportKpiExpression(models.Model):
             kpi = rec.kpi_id
             subkpi = rec.subkpi_id
             if subkpi:
-                name = u"{} / {} ({}.{})".format(
+                name = "{} / {} ({}.{})".format(
                     kpi.description, subkpi.description, kpi.name, subkpi.name
                 )
             else:
