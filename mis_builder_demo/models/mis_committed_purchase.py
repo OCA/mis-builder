03e8de89ab007cfd00ba6fac781cf115ff9df7d5
Dũng (Trần Đình) <dungtd@trobz.com>
2023-03-03 10:42:47 +0700

    [16.0][MIG] mis_builder_demo: Migration to 16.0

--- mis_builder_demo/models/mis_committed_purchase.py
+++ mis_builder_demo/models/mis_committed_purchase.py
@@ -14,9 +14,6 @@ class MisCommittedPurchase(models.Model):
 
     line_type = fields.Char()
     name = fields.Char()
-    analytic_account_id = fields.Many2one(
-        comodel_name="account.analytic.account", string="Analytic Account"
-    )
     account_id = fields.Many2one(comodel_name="account.account", string="Account")
     company_id = fields.Many2one(comodel_name="res.company", string="Company")
     credit = fields.Float()
@@ -27,41 +24,57 @@ class MisCommittedPurchase(models.Model):
     res_id = fields.Integer(string="Resource ID")
     res_model = fields.Char(string="Resource Model Name")
 
-    analytic_tag_ids = fields.Many2many(
-        comodel_name="account.analytic.tag",
-        relation="mis_committed_purchase_tag_rel",
+    analytic_account_ids = fields.Many2many(
+        comodel_name="account.analytic.account",
+        relation="mis_committed_purchase_analytic_account_rel",
         column1="mis_committed_purchase_id",
-        column2="account_analytic_tag_id",
-        string="Analytic Tags",
+        column2="analytic_account_id",
+        string="Analytic Accounts",
     )
 
     def init(self):
         script = opj(
             dirname(dirname(__file__)), "examples", "mis_committed_purchase.sql"
         )
+
         with open(script) as f:
             tools.drop_view_if_exists(self.env.cr, "mis_committed_purchase")
             self.env.cr.execute(f.read())
 
-            # Create many2many relation for account.analytic.tag
-            tools.drop_view_if_exists(self.env.cr, "mis_committed_purchase_tag_rel")
+            # Create many2many relation for account.analytic.account
+            tools.drop_view_if_exists(
+                self.env.cr, "mis_committed_purchase_analytic_account_rel"
+            )
             self.env.cr.execute(
                 """
-            CREATE OR REPLACE VIEW mis_committed_purchase_tag_rel AS
+            CREATE OR REPLACE VIEW mis_committed_purchase_analytic_account_rel AS
             (SELECT
                 po_mcp.id AS mis_committed_purchase_id,
-                po_rel.account_analytic_tag_id AS account_analytic_tag_id
-            FROM account_analytic_tag_purchase_order_line_rel AS po_rel
-            INNER JOIN mis_committed_purchase AS po_mcp ON
-                po_mcp.res_id = po_rel.purchase_order_line_id
-            WHERE po_mcp.res_model = 'purchase.order.line'
+                jsonb_object_keys(po_rel.analytic_distribution)::INTEGER
+                    AS analytic_account_id
+
+            FROM
+                purchase_order_line AS po_rel
+                INNER JOIN mis_committed_purchase AS po_mcp
+                    ON po_mcp.res_id = po_rel.id
+
+            WHERE
+                po_mcp.res_model = 'purchase.order.line'
+
             UNION ALL
+
             SELECT
                 inv_mcp.id AS mis_committed_purchase_id,
-                inv_rel.account_analytic_tag_id AS account_analytic_tag_id
-            FROM account_analytic_tag_account_move_line_rel AS inv_rel
-            INNER JOIN mis_committed_purchase AS inv_mcp ON
-                inv_mcp.res_id = inv_rel.account_move_line_id
-            WHERE inv_mcp.res_model = 'account.move.line')
+                jsonb_object_keys(inv_rel.analytic_distribution)::INTEGER
+                    AS analytic_account_id
+
+            FROM
+                account_move_line AS inv_rel
+                INNER JOIN mis_committed_purchase AS inv_mcp ON
+                    inv_mcp.res_id = inv_rel.id
+
+            WHERE
+                inv_mcp.res_model = 'account.move.line'
+            )
             """
             )
--- mis_builder_demo/readme/CONTRIBUTORS.rst
+++ mis_builder_demo/readme/CONTRIBUTORS.rst
@@ -1,2 +1,4 @@
 * Stéphane Bidoul <stephane.bidoul@acsone.eu>
 * Arnaud Pineux <arnaud.pineux@acsone.eu>
+* `Trobz <https://trobz.com>`_:
+    * Dzung Tran <dungtd@trobz.com>
