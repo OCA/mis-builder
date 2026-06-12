bb62c4e29863b2330c572e8892dbf8f5a196d782
Pierre Verkest <pierre@verkest.fr>
2026-06-12 14:51:22 +0200

    [FIX] mis_builder: avoid warning in test and load the generated workbook to ensure number of sheets

--- mis_builder/tests/test_mis_report_instance.py
+++ mis_builder/tests/test_mis_report_instance.py
@@ -6,6 +6,7 @@ from odoo.tools import test_reports
 
 from ..models.accounting_none import AccountingNone
 from ..models.mis_report import TYPE_STR, SubKPITupleLengthError, SubKPIUnknownTypeError
+from .common import try_xlsx_report
 
 
 class TestMisReportInstance(common.HttpCase):
@@ -585,29 +586,25 @@ class TestMisReportInstance(common.HttpCase):
 
     def test_xlsx(self):
         self.report_instance.export_xls()  # get action
-        with self.assertLogs("odoo.tools.test_reports", level="WARNING") as log_catcher:
-            test_reports.try_report(
-                self.env.cr,
-                self.env.uid,
-                "mis_builder.mis_report_instance_xlsx",
-                [self.report_instance.id],
-                report_type="xlsx",
-            )
-        self.assertIn(
-            'Report mis_builder.mis_report_instance_xlsx produced a "xlsx" chunk, '
-            "cannot examine it",
-            log_catcher.output[0],
+        excel_workbook = try_xlsx_report(
+            self.env.cr,
+            self.env.uid,
+            "mis_builder.mis_report_instance_xlsx",
+            [self.report_instance.id],
+            report_type="xlsx",
         )
+        self.assertEqual(len(excel_workbook.sheetnames), 1)
 
     def test_xlsx_multiple_instances(self):
         self.report_instance.export_xls()  # get action
-        test_reports.try_report(
+        excel_workbook = try_xlsx_report(
             self.env.cr,
             self.env.uid,
             "mis_builder.mis_report_instance_xlsx",
             [self.report_instance.id, self.report_instance_4.id],
             report_type="xlsx",
         )
+        self.assertEqual(len(excel_workbook.sheetnames), 2)
 
     def test_get_kpis_by_account_id(self):
         account_ids = (
